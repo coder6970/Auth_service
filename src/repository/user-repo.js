eeeb936abb2ec13754534839d6b02f43d12f872c
Abhinav-Shrivas <abhinavshrivas2401@gmail.com>
2025-09-06 19:42:05 +0530

    token logic added

--- src/repository/user-repo.js
+++ src/repository/user-repo.js
@@ -1,25 +1,36 @@
-const {User} = require('../models/index');
+const { User } = require("../models/index");
 
 class UserRepository {
-    async create(data){
-        try {
-          const response = await User.create(data);
-          return response;
-        } catch (error) {
-          console.log("something went wrong inside repository layer");
-          throw { error };
-        }
+  async create(data) {
+    try {
+      const response = await User.create(data);
+      return response;
+    } catch (error) {
+      console.log("something went wrong inside repository layer");
+      throw { error };
     }
+  }
 
-    async destroy(id) {
-        try {
-          await this.newModel.destroy({ where: { id } });
-          return true;
-        } catch (error) {
-          console.log("something went wrong inside repository layer");
-          throw { error };
-        }
-      }
+  async destroy(id) {
+    try {
+      await User.destroy({ where: { id } });
+      return true;
+    } catch (error) {
+      console.log("something went wrong inside repository layer");
+      throw { error };
+    }
+  }
+  async getById(id) {
+    try {
+      const user = await User.findByPk(id, {
+        attributes: ["email", "id"],
+      });
+      return user;
+    } catch (error) {
+      console.log("something went wrong inside repository layer");
+      throw { error };
+    }
+  }
 }
 
-module.exports = UserRepository
+module.exports = UserRepository;
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,7 @@
 const UserRepository = require("../repository/user-repo");
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const {JWT_KEY} = require('../config/serverconfig');
 class UserService {
   constructor() {
     this.userRepository = new UserRepository();
@@ -21,6 +23,34 @@ class UserService {
       return response;
     } catch (error) {
       console.log("something went wrong in the user service");
+      throw error;
+    }
+  }
+
+  createToken(user){
+    try {
+      const result = jwt.sign(user,JWT_KEY,{expiresIn : '1d'});
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in the token creation.");
+      throw error
+    }
+  }
+
+  verifyToken(token){
+    try {
+      const response =  jwt.verify(token,JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in the token verifivation.",error);
+      throw error;
+    }
+  }
+  verifyPassword(userPlainInputPassword,ecryptedPassword){
+    try {
+      return bcrypt.compareSync(userPlainInputPassword,ecryptedPassword);
+    } catch (error) {
+      console.log("Something went wrong in the password verification.");
       throw error;
     }
   }
